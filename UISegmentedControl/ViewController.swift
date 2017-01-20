0fa364a02c1ecacd236c296b53d24627e38f7c56
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 02:07:42 +0530

    Pulse  Graphs  Settings A UISegmentedControl object is a horizontal control made of multiple segments, each segment functioning as a discrete button. A segmented control affords a compact means to group together a number of controls.

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var selectedValue = UILabel()
+    var segmentedControlView = UISegmentedControl()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        selectedValue.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        view.addSubview(selectedValue)
+        selectedValue.text = "selected control : one"
+
+        segmentedControlView = UISegmentedControl (items: ["one","two","three"])
+        segmentedControlView.frame = CGRect(x: 60, y: 250, width: 200, height: 30)
+        segmentedControlView.selectedSegmentIndex = 0
+        segmentedControlView.tintColor = UIColor.blue
+        segmentedControlView.addTarget(self, action: #selector(segmentedValueChanged), for: .valueChanged)
+        self.view.addSubview(segmentedControlView)
+        
+    }
+    
+    func segmentedValueChanged(sender:UISegmentedControl!)
+    {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            selectedValue.text = "selected control : one"
+        case 1:
+            selectedValue.text = "selected control : two"
+        case 2:
+            selectedValue.text = "selected control : three"
+            
+        default:
+            break;
+        }
     }
 
     override func didReceiveMemoryWarning() {
